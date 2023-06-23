481ec616f4759ee5866de50d2854149cab10cfdd
Milyra35 <marie.richir35@gmail.com>
2023-06-23 14:29:20 +0000

    fin exercice rpg, plus que les bonus à faire

--- assets/js/classes/character.js
+++ assets/js/classes/character.js
@@ -23,22 +23,104 @@ class Character {
     }
     
     moveUp() {
+        let newPosition = document.querySelector(`.col-${this.#posX}.row-${this.#posY - 1}`);
         
+        if(this.#posY >= 0)
+        {
+            if(newPosition.classList.contains("water") || 
+               newPosition.classList.contains("tree") ||
+               newPosition.classList.contains("rock") ||
+               newPosition.classList.contains("stump") ||
+               this.#posY < 0)
+            {
+                return false;
+            }
+            else 
+            {
+                this.#posY--;
+                this.render();
+            }
+        }
     }
     
     moveDown() {
+        let newPosition = document.querySelector(`.col-${this.#posX}.row-${this.#posY + 1}`);
         
+        if(this.#posY <= 19)
+        {
+            if(newPosition.classList.contains("water") || 
+               newPosition.classList.contains("tree") ||
+               newPosition.classList.contains("rock") ||
+               newPosition.classList.contains("stump") ||
+               this.#posY > 19)
+            {
+                return false;
+            }
+            else 
+            {
+                this.#posY++;
+                this.render();
+            }
+        }
     }
     
     moveLeft() {
+        let newPosition = document.querySelector(`.col-${this.#posX - 1}.row-${this.#posY}`);
         
+        if(this.#posX >= 0)
+        {
+            if(newPosition.classList.contains("water") || 
+               newPosition.classList.contains("tree") ||
+               newPosition.classList.contains("rock") ||
+               newPosition.classList.contains("stump"))
+            {
+                return false;
+            }
+            else 
+            {
+                this.#posX--;
+                this.render();
+            }
+        }
     }
     
     moveRight() {
+        let newPosition = document.querySelector(`.col-${this.#posX + 1}.row-${this.#posY}`);
         
+        if(this.#posX <= 29)
+        {
+            if(newPosition.classList.contains("water") || 
+               newPosition.classList.contains("tree") ||
+               newPosition.classList.contains("rock") ||
+               newPosition.classList.contains("stump") ||
+               this.#posX > 29)
+            {
+                return false;
+            }
+            else 
+            {
+                this.#posX++;
+                this.render();
+            }
+        }
     }
     
     render() {
+        let sections = document.querySelectorAll("body section");
+        
+        for(let i=0; i<sections.length; i++)
+        {
+            if(sections[i].classList.contains("character"))
+            {
+                sections[i].classList.remove("character")
+            }
+        }
+        
+        let newPosition = document.querySelector(`.col-${this.#posX}.row-${this.#posY}`);
+        newPosition.classList.add("character");
+    }
+    
+    godMode() {
         
     }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,26 @@ import { Character } from "./classes/character.js";
 
 window.addEventListener("DOMContentLoaded", function(){
     initMap(20, 30);
+    
+    let newCharacter = new Character(19, 15);
+    newCharacter.render();
+    
+    window.addEventListener("keydown", function(event) {
+        if(event.code === "KeyW")
+        {
+            newCharacter.moveUp();
+        }
+        else if(event.code === "KeyS")
+        {
+            newCharacter.moveDown();
+        }
+        else if(event.code === "KeyA")
+        {
+            newCharacter.moveLeft();
+        }
+        else if(event.code === "KeyD")
+        {
+            newCharacter.moveRight();
+        }
+    })
 });
